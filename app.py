3feeba99136b51be04796600bd7eb62ac26860eb
melindali255 <silverstream255@yahoo.com>
2019-04-27 08:53:36 -0700

    Finish log in page

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, url_for
+import sqlite3
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def home():
+    user = request.form["username"]
+    pw = request.form["password"]
+    print("user logged in with: " + user + ", " + pw)
+    return "Good luck with your webserver!"
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+app.run()
